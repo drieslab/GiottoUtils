9122d8709a470912ba104b61f6e9439356a50778
George Chen <72078254+jiajic@users.noreply.github.com>
2024-05-22 00:26:13 -0400

    feat: `init_option()`

---
+++ R/options.R
@@ -0,0 +1,28 @@
+
+
+#' @name init_option
+#' @title Initialize an option
+#' @description Set an option only if it does not exist yet.
+#' @param x character. Option to initialize
+#' @param value value to set
+#' @examples
+#' init_option("dummy_option", 2)
+#' init_option("dummy_option", 5)
+#' getOption("dummy_option") # still 2
+#' @returns NULL invisibly
+#' @export
+init_option <- function(x, value) {
+    if (!is.null(options(x))) {
+        return(invisible())
+    }
+
+    setting <- list(value)
+    names(setting) <- x
+
+    options(setting)
+
+    return(invisible())
+}
+
+
+
