238d8c977358242d88cc0ada794983f179245842
George Chen <72078254+jiajic@users.noreply.github.com>
2025-02-13 18:44:36 -0500

    ci: remove test. get_args() does not cooperate with testthat

--- tests/testthat/test_prev_call.R
+++ tests/testthat/test_prev_call.R
@@ -7,18 +7,3 @@ test_that("get_args_list works", {
 
     expect_identical(b, list(y = 2, z = 3))
 })
-
-foo <- function(a, b) get_args(toplevel = 1)
-bar <- function() get_args() # default toplevel = 2
-baz <- function(x, y) bar()
-
-test_that("get_args() works", {
-    out <- foo(a = 1, b = 2)
-    expect_identical(out, c(a = "1", b = "2"))
-    out <- baz("a", "b")
-    expect_identical(out, c(x = "a", y = "b"))
-})
-
-rm(foo)
-rm(bar)
-rm(baz)
